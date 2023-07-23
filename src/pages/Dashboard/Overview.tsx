1f72e615f24f032f20af96af9eb2dc1c0973f808
Zora029 <zomick029@gmail.com>
2023-07-23 12:04:27 +0300

    Overview change

--- src/pages/Dashboard/Overview.tsx
+++ src/pages/Dashboard/Overview.tsx
@@ -2,7 +2,6 @@ import CardOne from '../../components/CardOne.tsx';
 import CardTwo from '../../components/CardTwo.tsx';
 import CardThree from '../../components/CardThree.tsx';
 import ChartOne from '../../components/ChartOne.tsx';
-import TableOne from '../../components/TableOne.tsx';
 interface Iinformation {
   totalIncome: number;
   totalIncomeAgo: number;
@@ -40,9 +39,6 @@ const Overview = () => {
 
       <div className="mt-4 grid grid-cols-1 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
         <ChartOne />
-        <div className="col-span-12 xl:col-span-8">
-          <TableOne />
-        </div>
       </div>
     </>
   );
